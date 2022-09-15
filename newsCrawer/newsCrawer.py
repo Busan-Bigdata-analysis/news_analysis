25715b49aecda16ae25d2598b1e332909ce143c4
yuganom <108312553+PLAYseung@users.noreply.github.com>
2022-09-15 15:08:54 +0900

    #1 기사 크롤링 및 데이터로 변환
    - 테스트한 정규식을 적용 시키고 기사가 삭제된 페이지 또한 제외 시킴
    - 획득한 텍스트를 csv 형태의 파일로 저장 할 수 있게 만듬
    - csv 내부 구조
        - column : 인덱스, 기사 본문

--- newsCrawer/newsCrawer.py
+++ newsCrawer/newsCrawer.py
@@ -2,23 +2,39 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 from selenium import webdriver
+import re
 
 def getNewsFullText(result):
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     wd = webdriver.Chrome('newsCrawer/chromedriver.exe',options=options)
 
+    # 뉴스 번호 
     for i in range(800000,800010):
         wd.get(f'https://kr.investing.com/news/economy/article-{i}')
-        time.sleep(0.5) #팝업 표시후 크롤링이 안되서 브라우저가 닫히는 것을 방지
+        # time.sleep(0.5) #팝업 표시후 크롤링이 안되서 브라우저가 닫히는 것을 방지
         
         try:
             html = wd.page_source
             soup = BeautifulSoup(html,'html.parser')
             fullText = soup.select('#leftColumn > div > p')
+
+            pattern = '[a-z][a-z0-9_]{4,}@(\w+)[.](\w+)'
+            patt = re.compile(pattern)
+            # 기사가 삭제되어 페이지가 존재하지 않을때 나오는 메세지
+            breakOption = '찾으시는 페이지가 여기에 없는 것 같습니다.'
+
+            news = ''
+
+            # 정규식을 사용해서 기사 아래에 오는 기자, 다른 뉴스 링크 등 데이터에 포함 되지 않게 처리
             for j in fullText:
-                print(j.text)
-            
+                if re.search(pattern,j.text) or j.text == breakOption:
+                    break
+                news += j.text + ' '
+
+            # 정상적으로 기사를 획득했을 때 추가            
+            if news:
+                result.append(news)
 
         except Exception as e:
             print(e)
@@ -28,8 +44,12 @@ def getNewsFullText(result):
 
 def main():
     result = []
-    print('newsCrawer'+'>>'*10)
     getNewsFullText(result)
+
+    newsFulltext = pd.DataFrame(result,columns=['newsfulltext'])
+
+    newsFulltext.to_csv('newsCrawer/data/newsData.csv', index=True, encoding='utf-8')
+    print(len(result))
 
 
 if __name__=='__main__':
