b3fa9c20ccebe1aa091487ff24ab9b05a26923cb
sseungE <50647602+sseungE@users.noreply.github.com>
2022-09-19 17:21:35 +0900

    220919 model-Flask
    1. Ainize Flask랑 연동
    2. shortResult 초기화

--- totalWeb/run.py
+++ totalWeb/run.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,render_template,jsonify
+from ainize import *
 # 남이 만든 모듈을 내가 가져다 사용
 # from model import predictMainStream
 
@@ -8,9 +9,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/short')
+@app.route('/short', methods=['POST'])
 def short():
-    pass
+    # shortText = summary_text_ids[0]
+    res = {
+        # 'msg': tokenizer.decode(summary_text_ids(request.form.get('msg'))[0]),
+        'msg': decoding_summary_text(summary_text_ids)
+    }
+    return jsonify(res)
 
 @app.route('/analisys')
 def analisys():
